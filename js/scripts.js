6453c382fb9ad74aa24119d96b3310fa9cb09767
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-08-30 15:38:39 +0600

    Rooms add page complete with responsiveness..

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,22 @@
-(function($){
-	$(document).ready(function() {	
-
+(function ($) {
+	$(document).ready(function () {
 		// Scroll to Top
-		jQuery('.scrolltotop').click(function(){
-			jQuery('html').animate({'scrollTop' : '0px'}, 400);
+		jQuery(".scrolltotop").click(function () {
+			jQuery("html").animate({ scrollTop: "0px" }, 400);
 			return false;
 		});
-		
-		jQuery(window).scroll(function(){
+
+		jQuery(window).scroll(function () {
 			var upto = jQuery(window).scrollTop();
-			if(upto > 500) {
-				jQuery('.scrolltotop').fadeIn();
+			if (upto > 500) {
+				jQuery(".scrolltotop").fadeIn();
 			} else {
-				jQuery('.scrolltotop').fadeOut();
+				jQuery(".scrolltotop").fadeOut();
 			}
 		});
 
-
-		
-
-				
-		
-		
-		
-		
-		
-		
-		
-		
+		jQuery("#toggler-icon").click(function () {
+			jQuery(".admin-menu-wrpper").toggleClass("menu-show");
+		});
 	});
 })(jQuery);
